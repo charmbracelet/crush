5790f9421558c162c3f14c33c3de2581723bbf24
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-08-27 14:33:10 -0300

    test: fix on windows
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/home/home_test.go
+++ internal/home/home_test.go
@@ -8,19 +8,19 @@ import (
 )
 
 func TestDir(t *testing.T) {
-	require.NotEmpty(t, Dir(), "home directory should not be empty")
+	require.NotEmpty(t, Dir())
 }
 
 func TestShort(t *testing.T) {
 	d := filepath.Join(Dir(), "documents", "file.txt")
-	require.Equal(t, "~/documents/file.txt", Short(d), "should replace home directory with ~")
-	ad := "/absolute/path/file.txt"
+	require.Equal(t, filepath.FromSlash("~/documents/file.txt"), Short(d))
+	ad := filepath.FromSlash("/absolute/path/file.txt")
 	require.Equal(t, ad, Short(ad))
 }
 
 func TestLong(t *testing.T) {
-	d := "~/documents/file.txt"
-	require.Equal(t, filepath.Join(Dir(), "documents", "file.txt"), Long(d), "should replace ~ with home directory")
-	ad := "/absolute/path/file.txt"
+	d := filepath.FromSlash("~/documents/file.txt")
+	require.Equal(t, filepath.Join(Dir(), "documents", "file.txt"), Long(d))
+	ad := filepath.FromSlash("/absolute/path/file.txt")
 	require.Equal(t, ad, Long(ad))
 }

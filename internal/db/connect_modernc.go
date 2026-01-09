d4ea13ed155ca719298f9bf223eeba408268a0a5
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-01-09 13:19:31 -0300

    fix: build tags
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- internal/db/connect_modernc.go
+++ internal/db/connect_modernc.go
@@ -1,4 +1,4 @@
-//go:build !openbsd && !netbsd && !android
+//go:build (darwin && (amd64 || arm64)) || (freebsd && (amd64 || arm64)) || (linux && (386 || amd64 || arm || arm64 || loong64 || ppc64le || riscv64 || s390x)) || (windows && (386 || amd64 || arm64))
 
 package db
 

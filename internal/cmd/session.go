4f29a14befe853d37e6108ac9575091db853c408
Christian Rocha <christian@rocha.is>
2026-03-06 15:52:12 -0500

    chore(cmd/session): make json the default for session list

--- internal/cmd/session.go
+++ internal/cmd/session.go
@@ -21,8 +21,6 @@ var sessionCmd = &cobra.Command{
 	Long:    "Manage Crush sessions including listing, viewing, and deleting sessions.",
 }
 
-var sessionListJSON bool
-
 var sessionListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all sessions",
@@ -46,7 +44,6 @@ var sessionLastCmd = &cobra.Command{
 }
 
 func init() {
-	sessionListCmd.Flags().BoolVar(&sessionListJSON, "json", false, "Output in JSON format")
 	sessionCmd.AddCommand(sessionListCmd)
 	sessionCmd.AddCommand(sessionShowCmd)
 	sessionCmd.AddCommand(sessionLastCmd)
@@ -78,30 +75,19 @@ func runSessionList(cmd *cobra.Command, _ []string) error {
 		return fmt.Errorf("failed to list sessions: %w", err)
 	}
 
-	if sessionListJSON {
-		output := make([]sessionJSON, len(list))
-		for i, s := range list {
-			output[i] = sessionJSON{
-				ID:       session.HashID(s.ID),
-				UUID:     s.ID,
-				Title:    s.Title,
-				Created:  time.Unix(s.CreatedAt, 0).Format(time.RFC3339),
-				Modified: time.Unix(s.UpdatedAt, 0).Format(time.RFC3339),
-			}
+	output := make([]sessionJSON, len(list))
+	for i, s := range list {
+		output[i] = sessionJSON{
+			ID:       session.HashID(s.ID),
+			UUID:     s.ID,
+			Title:    s.Title,
+			Created:  time.Unix(s.CreatedAt, 0).Format(time.RFC3339),
+			Modified: time.Unix(s.UpdatedAt, 0).Format(time.RFC3339),
 		}
-		enc := json.NewEncoder(cmd.OutOrStdout())
-		enc.SetEscapeHTML(false)
-		return enc.Encode(output)
 	}
-
-	for _, s := range list {
-		hash := session.HashID(s.ID)
-		createdAt := time.Unix(s.CreatedAt, 0)
-		relative := relativeTime(createdAt)
-		fmt.Printf("%s %q %s (%s)\n", hash[:7], s.Title, createdAt.Format(time.RFC3339), relative)
-	}
-
-	return nil
+	enc := json.NewEncoder(cmd.OutOrStdout())
+	enc.SetEscapeHTML(false)
+	return enc.Encode(output)
 }
 
 type sessionJSON struct {
@@ -110,49 +96,6 @@ type sessionJSON struct {
 	Title    string `json:"title"`
 	Created  string `json:"created"`
 	Modified string `json:"modified"`
-}
-
-func relativeTime(t time.Time) string {
-	d := time.Since(t)
-	switch {
-	case d < time.Minute:
-		return "just now"
-	case d < time.Hour:
-		mins := int(d.Minutes())
-		if mins == 1 {
-			return "1 minute ago"
-		}
-		return fmt.Sprintf("%d minutes ago", mins)
-	case d < 24*time.Hour:
-		hours := int(d.Hours())
-		if hours == 1 {
-			return "1 hour ago"
-		}
-		return fmt.Sprintf("%d hours ago", hours)
-	case d < 48*time.Hour:
-		return "yesterday"
-	case d < 7*24*time.Hour:
-		days := int(d.Hours() / 24)
-		return fmt.Sprintf("%d days ago", days)
-	case d < 30*24*time.Hour:
-		weeks := int(d.Hours() / 24 / 7)
-		if weeks == 1 {
-			return "1 week ago"
-		}
-		return fmt.Sprintf("%d weeks ago", weeks)
-	case d < 365*24*time.Hour:
-		months := int(d.Hours() / 24 / 30)
-		if months == 1 {
-			return "1 month ago"
-		}
-		return fmt.Sprintf("%d months ago", months)
-	default:
-		years := int(d.Hours() / 24 / 365)
-		if years == 1 {
-			return "1 year ago"
-		}
-		return fmt.Sprintf("%d years ago", years)
-	}
 }
 
 // resolveSessionID resolves a session ID that can be a UUID, full hash, or hash prefix.

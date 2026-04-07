62c50bcb8530aaa6b595ca6b0e1b0b153662e061
Bruno Krugel <brunokrugel@gmail.com>
2026-04-07 18:28:35 -0300

    Revert #2521

--- internal/cmd/schema.go
+++ internal/cmd/schema.go
@@ -3,10 +3,8 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"reflect"
 
 	"github.com/charmbracelet/crush/internal/config"
-	"github.com/charmbracelet/crush/internal/csync"
 	"github.com/invopop/jsonschema"
 	"github.com/spf13/cobra"
 )
@@ -17,14 +15,7 @@ var schemaCmd = &cobra.Command{
 	Long:   "Generate JSON schema for the crush configuration file",
 	Hidden: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		var reflector jsonschema.Reflector
-		csyncMapType := reflect.TypeOf(csync.Map[string, any]{})
-		reflector.Mapper = func(t reflect.Type) *jsonschema.Schema {
-			if t.Kind() == reflect.Struct && t.PkgPath() == csyncMapType.PkgPath() && len(t.Name()) > 4 && t.Name()[:4] == "Map[" {
-				return reflector.ReflectFromType(reflect.MapOf(t.Field(0).Type, t.Field(1).Type))
-			}
-			return nil
-		}
+		reflector := new(jsonschema.Reflector)
 		bts, err := json.MarshalIndent(reflector.Reflect(&config.Config{}), "", "  ")
 		if err != nil {
 			return fmt.Errorf("failed to marshal schema: %w", err)

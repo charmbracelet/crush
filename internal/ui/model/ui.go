c32ceb43093c359648a537df87ecbe845d2f55a5
Vadim Inshakov <vadiminshakov@gmail.com>
2026-05-07 03:09:15 +0500

    feat: add plan mode

--- internal/ui/model/ui.go
+++ internal/ui/model/ui.go
@@ -111,6 +111,13 @@ const (
 	uiChat
 )
 
+type uiInputMode uint8
+
+const (
+	uiInputModeCode uiInputMode = iota
+	uiInputModePlan
+)
+
 type openEditorMsg struct {
 	Text string
 }
@@ -205,6 +212,7 @@ type UI struct {
 
 	focus uiFocusState
 	state uiState
+	mode  uiInputMode
 
 	keyMap KeyMap
 	keyenh tea.KeyboardEnhancementsMsg
@@ -405,7 +413,8 @@ func New(com *common.Common, initialSessionID string, continueLast bool) *UI {
 		ui.themeKey = styles.ThemeKeyForProvider(cfg.Models[config.SelectedModelTypeLarge].Provider)
 	}
 
-	ui.setEditorPrompt(com.Workspace.PermissionSkipRequests())
+	ui.mode = uiInputModeCode
+	ui.setEditorPrompt(com.Workspace.PermissionSkipRequests(), ui.mode)
 	ui.randomizePlaceholders()
 	ui.textarea.Placeholder = ui.readyPlaceholder
 	ui.status = status
@@ -1641,7 +1650,7 @@ func (m *UI) handleDialogMsg(msg tea.Msg) tea.Cmd {
 	case dialog.ActionToggleYoloMode:
 		yolo := !m.com.Workspace.PermissionSkipRequests()
 		m.com.Workspace.PermissionSetSkipRequests(yolo)
-		m.setEditorPrompt(yolo)
+		m.setEditorPrompt(yolo, m.mode)
 		m.dialog.CloseDialog(dialog.CommandsID)
 	case dialog.ActionSelectNotificationStyle:
 		cfg := m.com.Config()
@@ -2226,6 +2235,10 @@ func (m *UI) handleKeyPressMsg(msg tea.KeyPressMsg) tea.Cmd {
 			}
 
 			switch {
+			case key.Matches(msg, m.keyMap.ShiftTab):
+				if cmd := m.toggleInputMode(); cmd != nil {
+					cmds = append(cmds, cmd)
+				}
 			case key.Matches(msg, m.keyMap.Editor.AddImage):
 				if !m.currentModelSupportsImages() {
 					break
@@ -2432,6 +2445,10 @@ func (m *UI) handleKeyPressMsg(msg tea.KeyPressMsg) tea.Cmd {
 			}
 		case uiFocusMain:
 			switch {
+			case key.Matches(msg, m.keyMap.ShiftTab):
+				if cmd := m.toggleInputMode(); cmd != nil {
+					cmds = append(cmds, cmd)
+				}
 			case key.Matches(msg, m.keyMap.Tab):
 				m.focus = uiFocusEditor
 				cmds = append(cmds, m.textarea.Focus())
@@ -2781,6 +2798,7 @@ func (m *UI) ShortHelp() []key.Binding {
 		binds = append(
 			binds,
 			tab,
+			k.ShiftTab,
 			commands,
 			k.Models,
 		)
@@ -2872,6 +2890,7 @@ func (m *UI) FullHelp() [][]key.Binding {
 		mainBinds = append(
 			mainBinds,
 			tab,
+			k.ShiftTab,
 			commands,
 			k.Models,
 			k.Sessions,
@@ -3365,6 +3384,7 @@ func (m *UI) setEditorPrompt(yolo bool) {
 		m.textarea.SetPromptFunc(4, m.yoloPromptFunc)
 		return
 	}
+
 	m.textarea.SetPromptFunc(4, m.normalPromptFunc)
 }
 
@@ -3415,6 +3435,30 @@ func (m *UI) bangPromptFunc(info textarea.PromptInfo) string {
 		return t.Editor.PromptBangDotsFocused.Render()
 	}
 	return t.Editor.PromptBangDotsBlurred.Render()
+}
+
+func (m *UI) toggleInputMode() tea.Cmd {
+	targetMode := uiInputModePlan
+	targetAgentID := config.AgentPlan
+	targetModeLabel := "plan"
+	if m.mode == uiInputModePlan {
+		targetMode = uiInputModeCode
+		targetAgentID = config.AgentCoder
+		targetModeLabel = "code"
+	}
+
+	return func() tea.Msg {
+		if err := m.com.Workspace.AgentSetMain(targetAgentID); err != nil {
+			return util.ReportError(err)()
+		}
+
+		m.mode = targetMode
+		m.setEditorPrompt(m.com.Workspace.PermissionSkipRequests(), m.mode)
+		if err := m.com.Workspace.UpdateAgentModel(context.Background()); err != nil {
+			return util.ReportError(err)()
+		}
+		return util.NewInfoMsg("input mode: " + targetModeLabel)
+	}
 }
 
 // closeCompletions closes the completions popup and resets state.

a277bd091c69527c3f628e52ca7060b4a1c9df1b
Brian Landau <brianjlandau@gmail.com>
2026-08-08 02:45:52 -0700

    feat(subagents): add subagents modal with Running and Library tabs

--- internal/ui/model/keys.go
+++ internal/ui/model/keys.go
@@ -85,6 +85,7 @@ type KeyMap struct {
 	Tab           key.Binding
 	ToggleYolo    key.Binding
 	ParentSession key.Binding
+	Subagents     key.Binding
 }
 
 func DefaultKeyMap() KeyMap {
@@ -124,6 +125,10 @@ func DefaultKeyMap() KeyMap {
 		ParentSession: key.NewBinding(
 			key.WithKeys("ctrl+up"),
 			key.WithHelp("ctrl+up", "go to parent session"),
+		),
+		Subagents: key.NewBinding(
+			key.WithKeys("ctrl+x"),
+			key.WithHelp("ctrl+x", "subagents"),
 		),
 	}
 
